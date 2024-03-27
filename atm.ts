9e1946b639b59e6cfa22d9425cb0a15791b464af
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-03-27 23:27:25 +0500

    Creat ATM Machin

---
+++ atm.ts
@@ -0,0 +1,58 @@
+#! /usr/bin/env node  
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let mypin = 246810;
+let mybalance = 50000;
+
+let pin = await inquirer.prompt([
+  {
+    name: "pinCode",
+    type: "number",
+    message: "Enter your Pin",
+  },
+]);
+
+if (pin.pinCode == mypin) {
+  console.log(chalk.bgBlue.bold("Correct pine code."));
+  console.log(chalk.bgMagenta.bold("\t WELCOM TO YOUR ACCOUNT"));
+  console.log(chalk.green.bold(`your current balance is ${mybalance}`));
+  let selection = await inquirer.prompt([
+    {
+      name: "transection",
+      type: "list",
+      message: "pleas select Operation",
+      choices: ["Withdraw", "check balance", "Fast Cash"],
+    },
+  ]);
+  if (selection.transection == "Withdraw") {
+    let Amount = await inquirer.prompt([
+      {
+        message: "Enter your Amount",
+        type: "number",
+        name: "amount",
+      },
+    ]);
+    if (Amount.amount > mybalance) {
+      console.log(chalk.bgGreen.bold("Insufficient Balance"));
+    } else {
+      console.log(chalk.bgBlue.bold("transection Successfull!.."));
+      mybalance -= Amount.amount;
+      console.log(chalk.bgCyan.bold(`your remaining balance is ${mybalance}`));
+    }
+  } else if (selection.transection == "check balance") {
+    console.log(chalk.bgBlue.bold(`your balance is : ${mybalance}`));
+  } else if (selection.transection == "Fast Cash") {
+    let answer = await inquirer.prompt({
+      message: "select your Amount",
+      type: "list",
+      name: "fastCash",
+      choices: ["5000", "10000", "20000", "30000", "40000", "50000"],
+    });
+    console.log(chalk.bgBlue.bold("transection successfull!.."));
+    mybalance -= answer.fastCash;
+    console.log(chalk.bgCyan.bold(`your remaining balance is ${mybalance}`));
+  }
+} else {
+  console.log(chalk.bgBlue.bold("Incorrect Password"));
+}
